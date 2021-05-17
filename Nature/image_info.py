7228d7bfb81d2a5e4218d9a7cd0e044f06223137
Jose Castro <chepe.cas12@hotmail.com>
2021-05-17 10:16:43 -0700

    Vandits Initial commit

---
+++ Nature/image_info.py
@@ -0,0 +1,17 @@
+image_info = [
+    {
+        "id": "R6356f2933ab14c659038aa316f5afc9d",
+        "title": "Nature 1",
+        "tags": ["", "", ""]
+    },
+    {
+        "id": "Rdd306d82bc9571dfea5793b4faa8e3b2",
+        "title": "Nature 2",
+        "tags": ["", ""]
+    },
+    {
+        "id": "Re528ca8a1dac419ff2b0071120ecc986",
+        "title": "Nature 3",
+        "tags": ["","", ""]
+    }
+]
